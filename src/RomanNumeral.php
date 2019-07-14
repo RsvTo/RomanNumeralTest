091414f7137091b542729b31cd9dc1d972d368d3
RsvTo <drowned@live.com>
2019-07-14 12:51:38 +0100

    Attempt
    I'm not entirely sure how to use Git properly & cannot seem to get this file to open on my pc, so I've had to attempt this here.

--- src/RomanNumeral.php
+++ src/RomanNumeral.php
@@ -1,9 +1,9 @@
 <?php
 namespace PhpNwSykes;
 
-class RomanNumeral
+class RomanNumeral  //Defines name of Class
 {
-    protected $symbols = [
+    protected $symbols = [  //Sets up an array of roman numerals with their corresponding integer values named $symbols
         1000 => 'M',
         500 => 'D',
         100 => 'C',
@@ -15,20 +15,28 @@ class RomanNumeral
 
     protected $numeral;
 
-    public function __construct(string $romanNumeral)
+    public function __construct(string $romanNumeral)   //Append the two
     {
-        $this->numeral = $romanNumeral;
+        $this->numeral = $romanNumeral; //Sets the value of each numeral to the roman numeral
     }
-
+    
     /**
      * Converts a roman numeral such as 'X' to a number, 10
      *
      * @throws InvalidNumeral on failure (when a numeral is invalid)
      */
-    public function toInt():int
+    public function toInt():int     //Houses the toInt functonality
+        $result = 5;                //declares a variable named result with the vlaue as 5, this should print 'V'..
+        $roman = $romanNumberal;    //Assigns the variable $romanNumeral to variable $roman
+        
     {
-        $total = 0;
-
-        return $total;
-    }
+        foreach ($symbols as $key => $value) {          //Start of foreach loop to loop through the numerals
+            while (strpos($symbols, $key) === 0) {          
+                $result += $value;
+                $symbols = substr($roman, strlen($key));
+    }   else {
+                @throwargs InvalidNumeral;
+            }
+            return $result;
 }
+        echo $result;
